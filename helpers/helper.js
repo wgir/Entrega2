3bda5f12f51030619b7877f544d06d3042a20947
wgiraldo <williamgustavo@gmail.com>
2019-03-26 18:12:50 -0300

    lista cursos y registra cursos acabado

---
+++ helpers/helper.js
@@ -0,0 +1,54 @@
+const hbs=require('hbs');
+const fs=require('fs');
+const path=require('path');
+const archivoJSON=path.join(__dirname,'../listadoCursos.json');
+listaCursos=[];
+hbs.registerHelper('registrarCurso',(_id,_nombre,_descripcion,_valor)=>{
+    listaCursos= require(archivoJSON);
+    //listaCursos= JSON.parse(fs.readFileSync('../listadoCursos.json'));
+    if(!listaCursos.find(l=>l.id==parseInt(_id)))
+    {
+        let curso={id:_id,nombre:_nombre,descripcion:_descripcion,valor:_valor,estado:'Disponible'}
+        listaCursos.push(curso);
+        guardarCursosEnJSON();
+        return 'Curso registrado exitosamente!!';
+    }else
+        return 'El id del curso ya se encuentra registrado :(';
+    
+});
+
+hbs.registerHelper('listarCursos',()=>{
+    listaCursos= require(archivoJSON);
+    //listaCursos= JSON.parse(fs.readFileSync('../listadoCursos.json'));
+    let texto="<table> \
+                <thead> \
+                    <th>Id</th> \
+                    <th>Nombre</th> \
+                    <th>Descripcion</th> \
+                    <th>Valor</th> \
+                    <th>Estado</th> \
+                </thead> \
+                <tbody>";
+
+                listaCursos.forEach(element => {
+        texto=texto+"<tr><td>"+element.id+"</td>"+
+                     "<td>"+element.nombre+"</td>"+
+                     "<td>"+element.descripcion+"</td>"+
+                     "<td>"+element.valor+"</td>"+
+                     "<td>"+element.estado+"</td>";
+
+    });
+                texto=texto+"</tbody>              </table>"
+    
+    return texto;
+});
+
+const guardarCursosEnJSON=()=>{
+    let datos=JSON.stringify(listaCursos);
+    console.log("guardando "+datos);
+    fs.writeFile(archivoJSON,datos,(err)=>{
+        if(err) throw (err);
+        console.log('Archivo creado con exito');
+    })
+
+}
--- src/app.js
+++ src/app.js
@@ -2,16 +2,30 @@ var express = require('express');
 var app = express();
 const path=require('path');
 const hbs=require('hbs');
+const bodyParser=require('body-parser');
+
+require('../helpers/helper');
+
 
 const directorioPublico=path.join(__dirname,'../public');
 const directorioPartials=path.join(__dirname,'../partials');
 hbs.registerPartials(directorioPartials);
+
 //app.use(express.static(__dirname+'/public'))
+app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(directorioPublico));
 app.set('view engine','hbs');
+
 app.get('/',(req,res)=>{
-    res.render('index',{nombre:'William'});
+    res.render('index',{nombre:'William', param1:1, param2:1});
 })
+
+app.post('/registrarCurso',(req,res)=>{
+    res.render('registrarCurso',{id:req.body.id,nombre:req.body.nombre,descripcion:req.body.descripcion,valor:req.body.valor,estado:req.body.estado});
+})
+
+
+
 console.log(__dirname);
 
 
