60ed9e6ac1e2730ad56548f0fa4e7507686e1bed
wgiraldo <williamgustavo@gmail.com>
2019-03-26 12:08:46 -0300

    con hbs

---
+++ src/app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+const path=require('path');
+const hbs=require('hbs');
+
+const directorioPublico=path.join(__dirname,'../public');
+//app.use(express.static(__dirname+'/public'))
+app.use(express.static(directorioPublico));
+app.set('view engine','hbs');
+app.get('/',(req,res)=>{
+    res.render('index',{nombre:'William'});
+})
+console.log(__dirname);
+
+
+ 
+app.listen(3000,()=>{ console.log('Escuchando en el puerto 3000')});
